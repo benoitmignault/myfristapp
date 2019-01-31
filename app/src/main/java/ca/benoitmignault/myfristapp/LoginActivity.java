65a848b8c2f3ff39a41b9e80b30c695a09766038
portable-PC\portable <b.mignault@gmail.com>
2019-01-30 20:14:53 -0500

    Initial commit

---
+++ app/src/main/java/ca/benoitmignault/myfristapp/LoginActivity.java
@@ -0,0 +1,34 @@
+package ca.benoitmignault.myfristapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
+    ProgressBar loginProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.btn_login);
+        loginProgress = findViewById(R.id.login_progrss);
+        loginProgress.setVisibility(View.INVISIBLE);
+
+        // Au moment de cliquer sur le bouton, une animation de avancement du login se déclanche
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginProgress.setVisibility(View.VISIBLE);
+                loginButton.setVisibility(View.INVISIBLE);
+                Toast.makeText(this,"allo", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
