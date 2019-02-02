d9b9b53615bcd5fade6917720f775ca87578d669
frantz256255-PC\frantz256255 <b.mignault@gmail.com>
2019-02-02 12:11:04 -0500

    ajout d ela connexion avec succes de Facebook

--- app/src/main/java/ca/benoitmignault/myfristapp/LoginActivity.java
+++ app/src/main/java/ca/benoitmignault/myfristapp/LoginActivity.java
@@ -1,11 +1,23 @@
 package ca.benoitmignault.myfristapp;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static android.content.pm.PackageManager.GET_SIGNATURES;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +32,11 @@ public class LoginActivity extends AppCompatActivity {
         loginProgress = findViewById(R.id.login_progrss);
         loginProgress.setVisibility(View.INVISIBLE);
 
+        printKeyHash();
+
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+
         // Au moment de cliquer sur le bouton, une animation de avancement du login se déclanche
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +47,21 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+    // Procedure pour récupérer le hash key code
+    private void printKeyHash(){
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo("ca.benoitmignault.myfristapp", GET_SIGNATURES);
+            for(Signature signature: info.signatures ){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/ca/benoitmignault/myfristapp/MainActivity.java
+++ app/src/main/java/ca/benoitmignault/myfristapp/MainActivity.java
@@ -2,6 +2,8 @@ package ca.benoitmignault.myfristapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
